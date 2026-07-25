0ce66746bb4f232689cb5f2fd7bd8cd60053b364
buffmio <laesunny@gmail.com>
2026-07-26 04:39:39 +0800

    test: match documentation keywords case-insensitively

--- packaging/test-docs.sh
+++ packaging/test-docs.sh
@@ -8,7 +8,7 @@ for text in \
   '10a5:9200' 'v0.1.0-wip' 'Arch Linux' 'Ubuntu 24.04' \
   'Ubuntu 26.04' 'Debian 13' 'Fedora 42' 'Fedora 43' \
   'fprintd-enroll' 'fprintd-verify' 'experimental' 'MR !570' 'LGPL-2.1'; do
-  rg -Fq "$text" "$readme"
+  rg -Fiq "$text" "$readme"
 done
 
 rg -q 'aur\.archlinux\.org/packages/libfprint-fpc1022' "$readme"
